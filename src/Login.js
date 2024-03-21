4753a81bbdd433db5e47ca5f383ddb828a70b167
ojieh david <130452537+ojiehdavid5@users.noreply.github.com>
2024-03-21 22:02:00 +0100

    changes

--- src/Login.js
+++ src/Login.js
@@ -12,6 +12,8 @@ const Login = () => {
     const [pwd,setPwd]=useState('');
     const [errMsg,setErrMsg]=useState('');
     const [success,setSuccess]=useState(false);
+    console.log(pwd);
+
 
 
     useEffect(()=>{
@@ -23,20 +25,81 @@ const Login = () => {
         setErrMsg('');
 
 
-    },[user,pwd]);
+    },[user,pwd])
+
+
+
+      const handleSubmit = async (e) => {
+        e.preventDefault();
+        console.log(user, pwd);
+        setUser('');
+        setPwd("");
+        setSuccess(true);
+      }
+
+
+    
+
+
+    
 
   return (
+    <>
+
+    {success?(
+      <section>
+        <h1>You are logged in</h1>
+        <br/>
+        <p>
+          <a href="github.com">Go to home</a>
+        </p>
+      </section>
+    ):(
+    
+    
     <section>
       <p ref={errRef} className={errMsg ? "errmsg":"offscreen"} aria-live="assertive">{errMsg}</p>
 
       <h1>Sign in</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <label htmlFor='username'>Username:</label>
+        <input
+        type="text"
+        id="username"
+        ref={userRef}
+        autoComplete="off"
+        onChange={(e)=>setUser(e.target.value)}
+        value={user}
+        required
         
+        />
+        <label htmlFor='username'>Password:</label>
+        <input
+        type="password"
+        id="password"
+        ref={userRef}
+        onChange={(e)=>setPwd(e.target.value)}
+        value={pwd}
+        required
+        
+        />
+        <button>Sign In</button>
+
       </form>
+      <p>
+        Need an Account?<br/>
+        <span className='line'>
+          {}
+          <a href="github.com">Sign Up</a>
+        </span>
+      </p>
       
     </section>
+
+  )
+}
+</>
   )
 }
 
-export default Login
+export default Login;

25befacf67e8998e4ecb9592f8b61e93a7e11f98
Ahnibaba <aniebietedet2025@gmail.com>
2024-04-10 18:18:08 +0100

    courseReg page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import Register from './Register';
 // import Login from './Login'
 import MainPage from './MainPage'
+import CourseRegistration from './CourseRegistration';
 
 function App() {
 
   return (
     <main className="App">
-      <Register/>
+      {/* <Register/> */}
       {/* <Login/> */}
+      <CourseRegistration />
       <MainPage/>
     </main>
   );
---
+++ src/CourseRegistration.js
@@ -0,0 +1,63 @@
+import React from "react";
+// import Nav from "./Nav";
+import "./courseRegistration.css";
+
+const CourseRegistration = () => {
+  return (
+    <>
+     
+      <div className="center">
+        <div className="registrationForm">
+          <h1>Student Course Registration</h1>
+          <hr></hr>
+          <form action="" className=".form">
+            <label htmlFor="studentDetails">Student's Details</label>
+            <div className="detailsContainer">
+              <input placeholder="Surname" type="text" />
+              <input placeholder="First name" type="text" />
+              <input placeholder="Other name" type="text" />
+              <input placeholder="Matriculation Number" type="text" />
+              <input placeholder="Registration Number" type="text" />
+              <input placeholder="Department" type="text" />
+              <input placeholder="Level" type="text" />
+              <input placeholder="Semester" type="text" />
+              <input placeholder="Faculty" type="text" />
+            </div>
+
+            <label htmlFor="courses">Course</label>
+            <div className="courseContainer">
+              <input placeholder="course" type="text" />
+              <input placeholder="course" type="text" />
+              <input placeholder="course" type="text" />
+              <input placeholder="course" type="text" />
+              <input placeholder="course" type="text" />
+              <input placeholder="course" type="text" />
+              <input placeholder="course" type="text" />
+              <input placeholder="course" type="text" />
+              <input placeholder="course" type="text" />
+              <input placeholder="course" type="text" />
+              <input placeholder="course" type="text" />
+              <input placeholder="course" type="text" />
+              <input placeholder="course" type="text" />
+              <input placeholder="course" type="text" />
+              <input placeholder="course" type="text" />
+            </div>
+
+            <h2>Electives</h2>
+            <p>
+              Elective 1<input type="radio" />
+            </p>
+
+            <p>
+              Elective 2<input type="radio" />
+            </p>
+
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CourseRegistration;
